93e8276fd5b088f3f53599f015da575d3fe83ec0
Cekiso <nonkululeko@quirky30.co.za>
2022-07-14 01:46:59 +0200

    html

--- client/homework.js
+++ client/homework.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export default function moviesApp() {
+export default function homeworkApp() {
 
     return {
 
--- client/main.js
+++ client/main.js
@@ -3,6 +3,9 @@ import './index.css'
 
 import Alpine from 'alpinejs'
 
+import homeworkApp from './homework'
+Alpine.data('enterOpen', homeworkApp)
+
 window.Alpine = Alpine
 
 Alpine.start()
