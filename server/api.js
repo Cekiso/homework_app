2ab64e2436a092e5dfae8b29aa45bfa1a0c78643
andiswa-sinxo <andiswasinxo@gmail.com>
2022-07-15 14:18:05 +0200

    aaaa

--- server/api.js
+++ server/api.js
@@ -1,9 +1,13 @@
 const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+
 
 module.exports = function name(app, db) {
+    
     app.get('/api/test', function(req, res) {
         res.json({
             name: 'Student'
         });
     });
 }
+
