7c0772053c02bd2b0c0b6358afd8abfdf3b16110
Tphohlela <17thapho@gmail.com>
2022-07-17 20:24:43 +0200

    added logic to add topic

--- client/homework.js
+++ client/homework.js
@@ -5,8 +5,15 @@ export default function homeworkApp() {
     return {
 
         addedSubject: null,
+        addedTopic:null,
         addSubjectSection: true,
+        topicSection:false,
         subjectsList: [],
+        subjectname:null,
+        topicsList:[],
+        topicname:null,
+        addQuestionSection:false,
+
 
         addSubject() {
             console.log('checking subject' + this.addedSubject)
@@ -31,8 +38,33 @@ export default function homeworkApp() {
                  
                     this.subjectsList = result.data.data
                 })
-        }
+        },
 
+        addTopics(){
+            console.log('hey'+ this.subjectname + this.addedTopic)
+              const subject = this.subjectname
+              const topic = this.addedTopic
+
+            axios
+                .post('http://localhost:8585/api/addTopics', { subject,topic })
+                .then((result) => {
+                    console.log('checking added topics'+ JSON.stringify(result.data))
+                })
+        },
+
+        displayTopics() {
+            console.log('oooo'+ this.subjectname)
+            const subject = this.subjectname
+
+            axios
+                .get(`http://localhost:8585/api/topics/${subject}`)
+                .then((result) => {
+                    console.log('checking' + result.data)
+                    console.log('checking topics' + JSON.stringify(result.data))
+                 
+                    this.topicsList = result.data.data
+                })
+        },
     }
 }
 //Multiple alpinex-data
