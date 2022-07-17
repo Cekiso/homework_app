d17799193ab85a099b56d532d74d5978e21fbac5
Tphohlela <17thapho@gmail.com>
2022-07-17 16:56:13 +0200

    added alpine to subjects screen

--- client/homework.js
+++ client/homework.js
@@ -4,24 +4,53 @@ export default function homeworkApp() {
 
     return {
 
+        addedSubject: null,
+        addSubjectSection: true,
+        subjectsList: [],
+
+        addSubject() {
+            console.log('checking subject' + this.addedSubject)
+
+            const subject = this.addedSubject
+
+            axios
+                .post('http://localhost:8585/api/addSubjects', { subject })
+                .then((result) => {
+                    console.log(result.data)
+
+                })
+
+        },
+
+        displaySubjects() {
+            axios
+                .get(`http://localhost:8585/api/subjects`)
+                .then((result) => {
+                    console.log(result.data.data)
+                    console.log('jjj' + JSON.stringify(result.data.data))
+                 
+                    this.subjectsList = result.data.data
+                })
+        }
+
     }
 }
 //Multiple alpinex-data
-document.addEventListener('alpine:init', () => {
-    Alpine.data('subject', () => ({
-        init() {
-            this.addSubject()
-        },
-        //it should return this 
-        addedSubject: '',
+// document.addEventListener('alpine:init', () => {
+//     Alpine.data('subject', () => ({
+//         init() {
+//             this.addSubject()
+//         },
+//         //it should return this
+//         addedSubject: '',
 
-        addSubject() {
-            axios
-                .post("http://localhost:8585/api/subject", this.addedSubject)
-                .then((App) => {
-                    console.log(App.data);
-                })
-        }
-    }))
+//         addSubject() {
+//             axios
+//                 .post("http://localhost:8585/api/subject", this.addedSubject)
+//                 .then((App) => {
+//                     console.log(App.data);
+//                 })
+//         }
+//     }))
 
-})
+// })
--- client/main.js
+++ client/main.js
@@ -1,11 +1,9 @@
 import './style.css'
 import './index.css'
-
 import Alpine from 'alpinejs'
-
 import homeworkApp from './homework'
-Alpine.data('enterOpen', homeworkApp)
 
 window.Alpine = Alpine
-
+Alpine.data('homework', homeworkApp)
 Alpine.start()
+
