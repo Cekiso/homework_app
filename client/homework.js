5a841eafa85fed0fe39bf5dc91991d19ec27c4d6
Cekiso <nonkululeko@quirky30.co.za>
2022-07-26 01:04:56 +0200

    landing

--- client/homework.js
+++ client/homework.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default function homeworkApp() {
 
     return {
-
+        teachersLandingPage:true,
         addedSubject: null,
         addedTopic: null,
         addSubjectSection: false,
